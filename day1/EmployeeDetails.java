3db778e80498135f5f5062ebeafc94d18cf7dc0e
Abishek0409 <abishek04.aa@gmail.com>
2025-10-12 14:06:41 +0530

    Add files via upload
    Day1 Home Assinments

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,31 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name ="John Doe";
+		byte age = 30;
+		float salary = 55000.75f;
+		char gender = 'M';
+		boolean acceptTerms =true;
+		float rating = 4.5f;
+		long mobilenumber = 9876543210L;
+		int pincode =56001;
+		char luckynumber ='7';
+		System.out.println("Employee Name is:"+name+"\n "+"Age is:"+age+"\n"+"Salary is:"+salary+"\n "+"gender is:"+gender+"\n "+"Accept terms:"+acceptTerms+"\n "+
+				"rating:"+rating+"\n "+"Mobile number:"+mobilenumber+"\n "+"Pincode:"+pincode+"\n "+"lucky number is:"+luckynumber);
+		
+		
+		
+		
+				
+				
+		
+		
+				
+				
+
+	}
+
+}
